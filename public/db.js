72cefb149f23a313e2dd75540d98c66aa8f61838
Mr Sam Law <sam@chongland.com>
2021-07-09 14:45:29 +1000

    IndexedDB functions

--- public/db.js
+++ public/db.js
@@ -16,3 +16,77 @@ request.onupgradeneeded = function (e) {
     if (db.objectStoreNames.length === 0)
         db.createObjectStore('BudgetStore', { autoIncrement: true });
 };
+
+request.onerror = function (e) {
+    console.log(e.target.errorCode);
+};
+
+function checkDatabase() {
+    console.log('Check DB invoked');
+
+    // Open transaction on BudgetStore DB
+    let transaction = db.transaction(['BudgetStore'], 'readwrite');
+
+    // Access BudgetStore object
+    const store = transaction.objectStore('BudgetStore');
+
+    // Get all records from store and set to a variable
+    const getAll = store.getAll();
+
+    // If request was successful
+    getAll.onsuccess = function () {
+        // Bulk Add Store items when online
+        if (getAll.result.length > 0) {
+            fetch('/api/transaction/bulk', {
+                method: 'POST',
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    Accept: 'application/json, text/plain, */*',
+                    'Content-Type': 'application/json',
+                },
+            })
+                .then((response) => response.json())
+                .then((res) => {
+                    // If our returned response is not empty
+                    if (res.length !== 0) {
+                        //Open another transaction to BudgetStore with read & write
+                        transaction = db.transaction(['BudgetStore'], 'readwrite');
+
+                        // Assign the current store to a variable
+                        const currentStore = transaction.objectStore('BudgetStore');
+
+                        // Clear existing entries after successful bulk add
+                        currentStore.clear();
+                        console.log('Clearing store');
+                    }
+                });
+        }
+    };
+}
+
+request.onsuccess = function (e) {
+    console.log('Success');
+    db = e.target.result;
+
+    // Check if app is online before reading from db
+    if (navigator.onLine) {
+        console.log('Backend online!');
+        checkDatabase();
+    }
+};
+
+const saveRecord = (record) => {
+    console.log('Save record invoked');
+
+    // Create a transaction on the BudgetStore db with readwrite access
+    const transaction = db.transaction(['BudgetStore'], 'readwrite');
+
+    // Access BudgetStore
+    const store = transaction.objectStore('BudgetStore');
+
+    // Add record to store with add method.
+    store.add(record);
+};
+
+// Listen for app coming back online
+window.addEventListener('online', checkDatabase);
