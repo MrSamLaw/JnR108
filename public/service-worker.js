be7c64e0b1dfa93b8b05b606979a1bcb1036c062
Mr Sam Law <sam@chongland.com>
2021-07-10 13:08:46 +1000

    Start service-worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,1 +1,42 @@
-console.log("Service Worker.js");
+const FILES_TO_CACHE = [
+    "/",
+    "/index.html",
+    "/index.js",
+    "/db.js",
+    "/styles.css",
+    "/manifest.webmanifest",
+    "/icons/JnR108-512.png",
+];
+
+const CACHE_NAME = "static-cache-v2";
+const DATA_CACHE_NAME = "data-cache-v1";
+
+// Install
+self.addEventListener("install", function (evt) {
+    evt.waitUntil(
+        caches
+            .open(CACHE_NAME)
+            .then(cache => cache.addAll(FILES_TO_CACHE))
+            .then(self.skipWaiting())
+    );
+});
+
+// Activate
+self.addEventListener("activate", function (evt) {
+    const currentCaches = [CACHE_NAME, DATA_CACHE_NAME];
+    evt.waitUntil(
+        caches
+            .keys()
+            .then((cacheNames) => {
+                return cacheNames.filter((cacheName) => !currentCaches.includes(cacheName));
+            })
+            .then((cachesToDelete) => {
+                return Promise.all(
+                    cachesToDelete.map((cacheToDelete) => {
+                        return caches.delete(cacheToDelete);
+                    })
+                );
+            })
+            .then(() => self.clients.claim())
+    );
+});
